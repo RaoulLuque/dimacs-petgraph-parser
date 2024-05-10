af3dc3d242b6ad571e52e2a42ea6aa8407342832
Raoul Luqué <raoulsluque@gmail.com>
2024-05-10 16:58:53 +0200

    Updated parser to read in optional information about treewidth

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Dimacs Petgraph Parser
 
-Basic parser to convert graphs in [dimacs .col](http://lcs.ios.ac.cn/~caisw/Resource/about_DIMACS_graph_format.txt) format to [petgraphs](https://github.com/petgraph/petgraph).
+Basic parser to convert graphs in [dimacs .col](http://lcs.ios.ac.cn/~caisw/Resource/about_DIMACS_graph_format.txt) format to [petgraphs](https://github.com/petgraph/petgraph) with optional treewidth information. The treewidth information is in optional lines in the following order in between the c and p lines in the dimacs.col file:
+```
+t TREEWIDTH_NUMBER
+l TREEWIDTH_LOWER_BOUND_NUMBER
+u TREEWIDTH_UPPER_BOUND_NUMBER
+```
+(see examples in [test_graphs](test_graphs))
 
 # Usage / Example
 
@@ -14,7 +20,12 @@ Use the read_graph() function. This will return a result with an error if any IO
 ```rust
   let test_graph_one_file =
       File::open("test_graphs/test_graph_one.col").expect("Should be able to read file");
-  let test_graph_one: Graph<i32, i32, petgraph::prelude::Undirected> =
+  let (test_graph_one, treewidth, lower_bound, upper_bound): 
+        (Graph<i32, i32, petgraph::prelude::Undirected>,
+        Option<usize>,
+        Option<usize>,
+        Option<usize>
+        ) =
       read_graph(test_graph_one_file)
           .expect("Test Graph One should be readable/in correct format");
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
 use petgraph::Graph;
 use std::io::{BufRead, BufReader, Read};
 
+/// Basic parser to convert graphs in [dimacs .col](http://lcs.ios.ac.cn/~caisw/Resource/about_DIMACS_graph_format.txt)
+/// format to [petgraphs](https://github.com/petgraph/petgraph) with optional treewidth information.
+///
+/// The returned result contains:
+/// (graph, treewidth (if contained in file), lower bound on treewidth (if contained in file), upper bound on treewidth (if contained in file)) (if no error occurred)
 pub fn read_graph<N: Clone + Default, E: Clone + Default, R: Read>(
     file: R,
-) -> Result<Graph<N, E, petgraph::prelude::Undirected>, Box<dyn std::error::Error>> {
+) -> Result<
+    (
+        Graph<N, E, petgraph::prelude::Undirected>,
+        Option<usize>,
+        Option<usize>,
+        Option<usize>,
+    ),
+    Box<dyn std::error::Error>,
+> {
     let mut graph: Graph<N, E, petgraph::prelude::Undirected> = Graph::new_undirected();
 
     let lines: Result<Vec<_>, _> = BufReader::new(file).lines().collect();
@@ -15,9 +28,55 @@ pub fn read_graph<N: Clone + Default, E: Clone + Default, R: Read>(
         .iter()
         .filter(|l| l.len() > 1 && l.chars().next() != Some('c'));
 
-    // Get header line
-    let header_line = lines.next().unwrap();
-    let header: Vec<&str> = header_line.split_whitespace().collect();
+    // Check if there is information about the treewidth
+    let mut treewidth = None;
+    let mut lower_bound = None;
+    let mut upper_bound = None;
+    let mut header_line_option = None;
+
+    while header_line_option == None {
+        let mut current_line = lines
+            .next()
+            .expect("File should contain more than just comments");
+        match current_line.chars().next() {
+            Some('t') => {
+                let treewidth_line: Vec<&str> = current_line.split_whitespace().collect();
+                treewidth = Some(
+                    treewidth_line
+                        .get(1)
+                        .expect("Treewidth line at header is not of correct format: t TREEWIDTH")
+                        .parse::<usize>()
+                        .expect("Treewidth line at header is not of correct format: t TREEWIDTH"),
+                );
+            }
+            Some('l') => {
+                let lower_bound_line: Vec<&str> = current_line.split_whitespace().collect();
+                lower_bound = Some(
+                    lower_bound_line
+                        .get(1)
+                        .expect("Treewidth line at header is not of correct format: t TREEWIDTH")
+                        .parse::<usize>()
+                        .expect("Treewidth line at header is not of correct format: t TREEWIDTH"),
+                );
+            }
+            Some('u') => {
+                let upper_bound_line: Vec<&str> = current_line.split_whitespace().collect();
+                upper_bound = Some(
+                    upper_bound_line
+                        .get(1)
+                        .expect("Treewidth line at header is not of correct format: t TREEWIDTH")
+                        .parse::<usize>()
+                        .expect("Treewidth line at header is not of correct format: t TREEWIDTH"),
+                );
+            }
+            a => {
+                assert_eq!(a.unwrap(), 'p', "File header is not of the correct format (one p(roperties) line after c(omments), t(treewidth), u(pper bound), l(ower bound) lines)");
+                header_line_option = Some(current_line);
+            }
+        }
+    }
+
+    let header: Vec<&str> = header_line_option.unwrap().split_whitespace().collect();
 
     let number_of_vertices: usize = header
         .get(2)
@@ -61,7 +120,7 @@ pub fn read_graph<N: Clone + Default, E: Clone + Default, R: Read>(
     }
 
     debug_assert_eq!(graph.edge_count(), number_of_edges);
-    Ok(graph)
+    Ok((graph, treewidth, lower_bound, upper_bound))
 }
 
 #[cfg(test)]
@@ -74,12 +133,19 @@ mod tests {
     fn test_read_graph_test_graph_one() {
         let test_graph_one_file =
             File::open("test_graphs/test_graph_one.col").expect("Should be able to read file");
-        let test_graph_one: Graph<i32, i32, petgraph::prelude::Undirected> =
-            read_graph(test_graph_one_file)
-                .expect("Test Graph One should be readable/in correct format");
+        let (test_graph_one, treewidth, lower_bound, upper_bound): (
+            Graph<i32, i32, petgraph::prelude::Undirected>,
+            Option<usize>,
+            Option<usize>,
+            Option<usize>,
+        ) = read_graph(test_graph_one_file)
+            .expect("Test Graph One should be readable/in correct format");
 
         assert_eq!(test_graph_one.edge_count(), 45);
         assert_eq!(test_graph_one.node_count(), 10);
+        assert_eq!(treewidth, None);
+        assert_eq!(lower_bound, None);
+        assert_eq!(upper_bound, Some(1000));
         for mut vertices in test_graph_one.node_indices().combinations(2) {
             let vertex_one = vertices.pop().expect("Graph should have vertices");
             let vertex_two = vertices.pop().expect("Graph should have vertices");
@@ -91,12 +157,19 @@ mod tests {
     fn test_read_graph_test_graph_two() {
         let test_graph_two_file =
             File::open("test_graphs/test_graph_two.col").expect("Should be able to read file");
-        let test_graph_two: Graph<i32, i32, petgraph::prelude::Undirected> =
-            read_graph(test_graph_two_file)
-                .expect("Test Graph One should be readable/in correct format");
+        let (test_graph_two, treewidth, lower_bound, upper_bound): (
+            Graph<i32, i32, petgraph::prelude::Undirected>,
+            Option<usize>,
+            Option<usize>,
+            Option<usize>,
+        ) = read_graph(test_graph_two_file)
+            .expect("Test Graph One should be readable/in correct format");
 
         assert_eq!(test_graph_two.edge_count(), 4);
         assert_eq!(test_graph_two.node_count(), 4);
+        assert_eq!(treewidth, Some(2));
+        assert_eq!(lower_bound, Some(2));
+        assert_eq!(upper_bound, None);
         for mut vertices in test_graph_two.node_indices().combinations(2) {
             let vertex_one = vertices.pop().expect("Graph should have vertices");
             let vertex_two = vertices.pop().expect("Graph should have vertices");
